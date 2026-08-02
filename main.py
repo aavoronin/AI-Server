cf4e02838ab77f7eb1c652d593b48a1f8357bc33
AV <a_voronin@list.ru>
2026-08-02 17:08:03 +0300

    benchmarks refactored

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ sys.path.insert(0, str(Path(__file__).parent / "ai-server"))
 
 from project_to_file.project_to_file import project_to_file_main
 from setup.copy_server_files import copy_server_files
-from setup.start_server import start_wsl_server, stop_wsl_server, run_model_benchmark
+from setup.start_server import start_wsl_server, stop_wsl_server
+from setup.run_model_benchmark import run_model_benchmark
 from test_server import ServerTester
 
 
